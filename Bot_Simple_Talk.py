ffb5377ba3e5575fe8cdf26a67fd2927f26b7c8d
Bjelanovic Mihajlo <127204343+Bjelanovic@users.noreply.github.com>
2024-06-22 17:57:18 +0200

    Create Bot_Simple_Talk.py

---
+++ Bot_Simple_Talk.py
@@ -0,0 +1,10 @@
+color = "blue"
+print(f"Oh, {color} is a beautiful color!")
+name= input("Hello! What is your name? ")
+print(f"Nice to meet you, {name}!")
+age_input=input("How old are you? ")
+bot_age = 3
+age=int(age_input)
+age_difference = age - bot_age
+print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+color=input("What's your favorite color? ")
